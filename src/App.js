02d5c2a0e6578e93b144f9c3e8daf6379b7860e6
kiqecanijo <enry1375@gmail.com>
2019-04-14 22:48:16 -0500

    imporving matrix function

--- src/App.js
+++ src/App.js
@@ -10,25 +10,25 @@ const Board = styled.div`
     margin:auto
 `
 
-const tiles = [...Array(size ** 2).keys()]
-
 class Game extends Component {
   constructor (props) {
+    const tiles = [...Array(size ** 2).keys()].sort((a, b) => 0.5 - Math.random())
     super(props)
     this.state = {
+      tiles,
       handle: null
     }
   }
 
   handleClick (number) {
-    asideInMatrix(number)
+    asideInMatrix(this.state.tiles, number) && console.log('done')
   }
 
   render () {
     return (
       <div className="App">
         <Board>
-          {tiles.map(number => <Box handleClick={this.handleClick} number={number} url={number ? `https://picsum.photos/200/200?image=${number}` : 'https://picsum.photos/g/200/300'}/>)}
+          {this.state.tiles.map(number => <Box handleClick={this.handleClick.bind(this)} number={number} url={number ? `https://picsum.photos/200/200?image=${number}` : 'https://picsum.photos/g/200/300'}/>)}
         </Board>
       </div>
     )
--- src/Utils.js
+++ src/Utils.js
@@ -1,6 +1,13 @@
 
 export const size = 3
-export const asideInMatrix = number => {
+export const asideInMatrix = (tiles, number) => {
   // TODO think in algoritm to know if its aside 0 number
-
+  const zeroIndex = tiles.indexOf(0)
+  const numberIndex = tiles.indexOf(number)
+  return (
+    numberIndex + 1 === zeroIndex ||
+    numberIndex - 1 === zeroIndex ||
+    numberIndex + size === zeroIndex ||
+    numberIndex - size === zeroIndex
+  )
 }
